1c8bf806d176d1113b7471fafc23a0c32e221550
JIWOOHWANG <wldn8108@gmail.com>
2023-05-30 17:58:25 +0900

    ...

---
+++ 1001.js
@@ -0,0 +1,8 @@
+// https://www.acmicpc.net/problem/1001
+
+const fs = require('fs');
+const input = fs.readFileSync("/dev/stdin").toString().trim().split(" ").map(Number);
+
+let a = input[0];
+let b = input[1];
+console.log(a-b);
--- 2557.js
+++ 2557.js
@@ -1,2 +1,3 @@
+//https://www.acmicpc.net/problem/2557
 let str ='Hello World!';
 console.log(str);
