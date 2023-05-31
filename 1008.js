e710a10596b5345f9e41f99473fed95fbe5fb228
JIWOO <116138163+wldn8108@users.noreply.github.com>
2023-05-31 15:13:06 +0900

    Delete 1008.js

--- 1008.js
+++
@@ -1,7 +0,0 @@
-//https://www.acmicpc.net/problem/1008
-const fs = require('fs');
-const input = fs.readFileSync("/dev/stdin").toString().trim().split(" ").map(Number);
-
-let a = input[0];
-let b = input[1];
-console.log(a/b);
