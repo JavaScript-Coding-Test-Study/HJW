f11cd445e746a58b149c79c07cfb95da673f33a3
JIWOO <116138163+wldn8108@users.noreply.github.com>
2023-05-31 15:13:42 +0900

    Delete 10998.js

--- 10998.js
+++
@@ -1,8 +0,0 @@
-//https://www.acmicpc.net/problem/10998
-
-const fs = require('fs');
-const input = fs.readFileSync("/dev/stdin").toString().trim().split(" ").map(Number);
-
-let a = input[0];
-let b = input[1];
-console.log(a*b);
